e3d34cc359168fed419751a668468ad73c8b44c9
Uros Nikolic <urosnikolic@Uross-MacBook-Pro.local>
2026-01-21 12:22:40 +0100

    lint fix

--- config/initializers/workflows.rb
+++ config/initializers/workflows.rb
@@ -1,7 +1,7 @@
 # Eager load workflow classes so they register in Base.registry
 # This is needed in development where eager_load is false
 Rails.application.config.to_prepare do
-  Dir[Rails.root.join('app/workflows/**/*_workflow.rb')].each do |file|
+  Dir[Rails.root.join("app/workflows/**/*_workflow.rb")].each do |file|
     require_dependency file
   end
 end
