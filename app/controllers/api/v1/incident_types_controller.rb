84b35e563dc81bffd41fbb182f0b0f73120103c1
Uros Nikolic <urosnikolic@Uross-MacBook-Pro.local>
2026-03-25 10:57:42 +0100

    public-api | refactor

--- app/controllers/api/v1/incident_types_controller.rb
+++ app/controllers/api/v1/incident_types_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::IncidentTypesController < Api::V1::ApiController
   def index
-    authorize!("incident_types", "read")
+    authorize!(ApiKey::RESOURCE_INCIDENT_TYPES, ApiKey::ACTION_READ)
     @incident_types = current_workspace.incident_types.active.ordered
   end
 end
