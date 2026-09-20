52d7670f9b6779ce48e643a87094a71e29333321
Uros Nikolic <unikolic94@gmail.com>
2026-09-20 13:34:58 +0200

    Let RubyLLM decide the endpoint and the model registry

--- engines/firefight_ai/lib/firefight_ai/engine.rb
+++ engines/firefight_ai/lib/firefight_ai/engine.rb
@@ -2,11 +2,6 @@ module FirefightAi
   class Engine < ::Rails::Engine
     isolate_namespace FirefightAi
 
-    # Keeps model lookups off ruby_llm_models, a copy no gem upgrade refreshes. Runs after RubyLLM's hook sets it.
-    initializer "firefight_ai.model_registry" do
-      ActiveSupport.on_load(:active_record) { RubyLLM.config.model_registry_store = nil }
-    end
-
     initializer "firefight_ai.configure_llm" do
       config.after_initialize do
         RubyLLM.configure do |c|
@@ -14,8 +9,6 @@ module FirefightAi
             c.public_send("#{setting}=", value) if value.present?
           end
           c.request_timeout = FirefightAi.configuration.request_timeout
-          # RubyLLM 2 defaults OpenAI to the Responses API, which OpenAI compatible bases may not serve.
-          c.openai_protocol = :chat_completions
         end
       end
     end
